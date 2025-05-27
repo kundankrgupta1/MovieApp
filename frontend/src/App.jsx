55e845e745ad288988535b7e0e7331df9703a16c
Kundan Kumar Gupta <kundankrgupta@outlook.com>
2025-05-28 01:12:13 +0530

    upload

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import AllRoutes from './Routes/AllRoutes'
 import Navbar from './Components/Navbar'
+// export const SERVER = "http://localhost:8080"
 export const SERVER = "http://localhost:8080"
 const App = () => {
 	return (
