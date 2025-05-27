bc91a6b09b52b9d3842a4a4e85a19f9adfea51ba
Kundan Kumar Gupta <kundankrgupta@outlook.com>
2025-05-28 01:17:47 +0530

    server change

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import AllRoutes from './Routes/AllRoutes'
 import Navbar from './Components/Navbar'
 // export const SERVER = "http://localhost:8080"
-export const SERVER = "http://localhost:8080"
+export const SERVER = "https://movieapp-1-lqt8.onrender.com"
 const App = () => {
 	return (
 		<div>
